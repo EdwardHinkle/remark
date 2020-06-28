8be76f778bfd37166dd1ef0d1416963c98cce2e8
Edward Hinkle <ehinkle@threatconnect.com>
2020-06-28 15:40:05 -0400

    Initialize app project

---
+++ Shared/RemarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemarkApp.swift
+//  Shared
+//
+//  Created by ehinkle-ad on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct RemarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
